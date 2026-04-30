b444ddbb0b7006f7e651141295791b9d574ac659
darovskakateryna <darovska.kateryna@e-u.edu.ua>
2026-04-30 21:05:38 +0300

    ...

--- practice-master/src/main.rs
+++ practice-master/src/main.rs
@@ -4,6 +4,7 @@ pub mod hackerrank {
     pub mod task05applesnoranges;
     pub mod task06kangaroo;
     pub mod task07betweentwosets;
+    pub mod task08breakingrecords;
 }
 fn main() {
     println!("Hello, world!");
